a51cb033c2276291f95cda0d59b3d062356e8af2
mfpop <56518631+mfpop@users.noreply.github.com>
2020-11-05 21:26:02 -0800

    first commit

---
+++ src/layouts/LayoutAdmin.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "antd";
+
+export default function LayoutAdmin() {
+  return (
+    <div>
+      <Layout>
+        <h2>Menu sider Admin</h2>
+        <div>Contenido..</div>
+        <h5>footer..</h5>
+      </Layout>
+    </div>
+  );
+}
---
+++ src/layouts/LayoutBasic.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "antd";
+
+export default function LayoutBasic() {
+  return (
+    <Layout>
+      <h2>Menu sider</h2>
+      <div>Contenido..</div>
+      <h5>footer..</h5>
+    </Layout>
+  );
+}
